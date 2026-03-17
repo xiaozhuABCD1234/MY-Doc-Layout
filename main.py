9d370836110f6a865c6fddee7d6339d97b6bef22
xiaozhu <xiaozhuABCD1234@163.com>
2026-03-17 20:15:28 +0800

    feat: 添加 YOLO26 和 YOLOv8 模型配置文件，更新主训练逻辑

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-def main():
-    print("Hello from my-doc-layout!")
+from ultralytics import YOLO
+from ultralytics.models.yolo.detect import DetectionTrainer
+from ultralytics.nn import DetectionModel
 
 
+class MyTrainer(DetectionTrainer):
+    def get_model(self, cfg=None, weights=None, verbose=True):
+        model: DetectionModel = super().get_model(cfg, weights, verbose)
+        return model
+
+
+# 使用自定义 trainer
 if __name__ == "__main__":
-    main()
+    trainer = MyTrainer(
+        overrides={
+            "model": "./yolo26n.yaml",
+            "data": "coco128.yaml",
+            "optimizer": "SGD",  # 强制使用 SGD
+            "lr0": 0.01,
+            "lrf": 0.01,  # 最终学习率 = lr0 * lrf = 0.0001
+            "momentum": 0.937,  # SGD 动量
+            "weight_decay": 0.0005,
+            # 训练配置
+            "epochs": 100,
+            "batch": 16,
+            "imgsz": 640,
+            "device": 0,
+            "warmup_epochs": 3,
+            "warmup_momentum": 0.8,
+            "warmup_bias_lr": 0.1,
+            # 数据增强（关闭 mosaic 过早可能影响小数据集）
+            "close_mosaic": 10,  # 最后10轮关闭 mosaic
+        }
+    )
+    trainer.train()
+
+# model = YOLO("/run/media/xiaozhu/移动/yolo/runs/detect/train10/weights/last.pt")
